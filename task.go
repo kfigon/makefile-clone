d4d78501cbc69dc8f780710c680979e3ed5facff
Kamil <kamil.figon@gmail.com>
2022-11-06 20:08:49 +0100

    more boiler

---
+++ task.go
@@ -0,0 +1,36 @@
+package main
+
+type action string
+
+type task struct{
+	name string
+	depedencies []task
+	actions []action
+}
+
+func newTask(name string, deps []task, actions []action) task{
+	return task{
+		name: name, 
+		depedencies: deps,
+		actions: actions,
+	}
+}
+
+
+type buildSystem struct {
+	tasks []task
+}
+
+func newBuildSystem() *buildSystem {
+	return &buildSystem{}
+}
+
+func (b *buildSystem) addTask(t task) {
+	b.tasks = append(b.tasks, t)
+}
+
+func (b *buildSystem) run() error {
+	// validate and run
+	return nil
+}
+
